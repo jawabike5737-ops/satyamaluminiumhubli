0541ec9c9be6c84b82c383959f2f6cf233ee5206
jawabike5737-ops <jawabike5737@gmail.com>
2026-04-09 09:29:05 +0530

    Delete satyam_project directory

--- satyam_project/satyam_project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include   # 👈 ADD include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')),   # 👈 ADD THIS LINE
-]
-
