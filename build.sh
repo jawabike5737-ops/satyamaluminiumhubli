5c2f92aac0daaef6bedab9c2f616fd1a2db36f82
jawabike5737-ops <jawabike5737@gmail.com>
2026-04-10 16:10:25 +0530

    force recreate tables

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
-python manage.py makemigrations
+
+python manage.py makemigrations core
+python manage.py migrate --fake-initial
 python manage.py migrate
+
 python manage.py collectstatic --noinput
