78bfa07f012a73b9ea3243b81dfba872b6d8b530
jawabike5737-ops <jawabike5737@gmail.com>
2026-04-11 17:31:32 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
 python -m pip install -r requirements.txt
-
-# Do NOT run makemigrations on the server. Create migrations locally.
-python manage.py migrate --noinput
 python manage.py collectstatic --noinput
